ddd2b75bacdf30c57b53007c26f9099cb2e64a57
Franziska Pätzold <Franziska.pae@gmail.com>
2019-03-15 09:05:43 +0100

    add 1 sec interval

--- public/ctech_video_creator.js
+++ public/ctech_video_creator.js
@@ -4,6 +4,7 @@
 
 let block_row1 = [];
 let block_row2 = [];
+let allBlocks = [];
 
 let placeholderSrc;
 
@@ -29,6 +30,7 @@ let colors = [color1,color2,color3,color4,color5,color6,color7,color8,color9,col
 
 //block canvases + images
 let canvasDataLists = [];
+
 //let socket = io();
 
 //####################################################################################################
@@ -48,12 +50,15 @@ function preload()
 function setup() 
 {
 	block_row1 = document.getElementById("block_row1").children;
+	allBlocks.push(block_row1);
 	block_row2 = document.getElementById("block_row2").children;
+	allBlocks.push(block_row2);
 
  	addPlaceholder(block_row1);
 	addPlaceholder(block_row2);
 	
-	colorFilter();
+	//sets color filters
+	moveContent();
 //TODO: correct resizing
 //	window.addEventListener("resize", resizeImages);
 }
@@ -157,41 +162,85 @@ function resizeImages()
 	}											
 }
 
-function colorFilter()
+function moveCanvases()
 {
+	
+	let id = setInterval(frame, 1000);
+	let index = 0;
+	function frame() 
+	{
+		//move picture
+
+		//move filter
+		colorFilter(index);
+		index++;
+
+	}
+}
+
+//TODO
+function moveContent()
+{
+	
+	let counter = 0;
+	//iterate trough canvases
 	for (let i=0; i < canvasDataLists.length; i++)
 	{
-		console.log(i);
+		let colorIndexOrigin =i;
 		let canvas = canvasDataLists[i][0];
 		let img = canvasDataLists[i][1];
 		let context = canvas.getContext("2d");
-		img.onload = function() 
-		{
-			let scaleFactor;
-			if (img.height>canvas.height)
-			{
-				scaleFactor = img.height/canvas.height;
-			}		
-			else
-			{
-				scaleFactor = canvas.height/img.height;
-			}
-			context.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
-				0, 0, img.width/scaleFactor, canvas.height); // destination rectangle
-
-			let imgData = context.getImageData(0,0,img.width,img.height);
-			let filterOpacity = 0.4;
-			for (let pixelIndex=0;pixelIndex<imgData.data.length;pixelIndex+=4)
-			{
-				imgData.data[pixelIndex] = imgData.data[pixelIndex] * (1-filterOpacity) + colors[i][0] * (filterOpacity); 
-				imgData.data[pixelIndex+1] =  imgData.data[pixelIndex] * (1-filterOpacity) + colors[i][1] * (filterOpacity); 
-				imgData.data[pixelIndex+2] =  imgData.data[pixelIndex] * (1-filterOpacity) + colors[i][2] * (filterOpacity); 
-				imgData.data[pixelIndex+3] = 255; 
-			}
-			context.putImageData(imgData,0, 0, 0,0, img.width, img.height);
-		} 
-
+		setInterval(function()
+				{
+					counter++;
+					console.log(counter);
+				}, 1000)
+			(counter);
+		colorFilter(canvas, img, context, colorIndexOrigin, counter);
 		
+	}	
+}
+
+
+function colorFilter(canvas, img, context,index, counter)
+{
+	let localColorIndex = index;
+	let helpIndex = index + counter;
+	if (helpIndex >= colors.length)
+	{
+		localColorIndex = helpIndex-colors.length;
+	}
+	else
+	{
+		localColorIndex = helpIndex;
+	}		
+	let currColor = colors[localColorIndex];
+
+	img.onload = function() 
+	{			
+		console.log("trigger");
+		let scaleFactor;
+		if (img.height>canvas.height)
+		{
+			scaleFactor = img.height/canvas.height;
+		}		
+		else
+		{
+			scaleFactor = canvas.height/img.height;
+		}
+		context.drawImage(img, 0, 0, img.width,    img.height,     // source rectangle
+			0, 0, img.width/scaleFactor, canvas.height); // destination rectangle
+
+		let imgData = context.getImageData(0,0,img.width,img.height);
+		let filterOpacity = 0.4;
+		for (let pixelIndex=0;pixelIndex<imgData.data.length;pixelIndex+=4)
+		{
+			imgData.data[pixelIndex] = imgData.data[pixelIndex] * (1-filterOpacity) + currColor[0] * (filterOpacity); 
+			imgData.data[pixelIndex+1] =  imgData.data[pixelIndex] * (1-filterOpacity) + currColor[1] * (filterOpacity); 
+			imgData.data[pixelIndex+2] =  imgData.data[pixelIndex] * (1-filterOpacity) + currColor[2] * (filterOpacity); 
+			imgData.data[pixelIndex+3] = 255; 
+		}
+		context.putImageData(imgData,0, 0, 0,0, img.width, img.height);
 	}
 }
 
